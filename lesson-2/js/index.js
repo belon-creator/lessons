26098702b184ce2df95663a527281965d35f3918
belon-creator <belon8911@gmail.com>
2020-04-23 19:54:57 +0300

    add 2nd lesson

---
+++ lesson-2/js/index.js
@@ -0,0 +1,51 @@
+// ========================================
+/* Создание */
+// ========================================
+// const arr1 = [];
+// const arr2 = [1, 2, 3, 4, 5, 'qweqwe'];
+// const arr3 = Array(1, 2, 3, 4, 5);
+// const arr4 = Array(10);
+
+// console.log(arr1);
+// console.log(arr2);
+// console.log(arr3);
+// console.log(arr4);
+// console.log(arr2[5]);
+
+// ========================================
+/* Массивы */
+// ========================================
+
+// const arr = ['qwejm', 'asdjk', 'zxcvl', 'zaq'];
+
+// const arr1 = arr;
+// console.log(arr);
+
+// arr[2] = 'azsxdcfv';
+// console.log(arr1);
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i += 1) {
+//   console.log(arr[i]);
+// }
+// console.log('=================================');
+// for (let i = arr.length - 1; i >= 0; i -= 1) {
+//   console.log(arr[i]);
+// }
+
+// ========================================
+/* Цикл for...of */
+// ========================================
+// const arr = ['Bobby', 'John', 'Chris', 'Admin'];
+
+// for (const name of arr) {
+//   if (name === 'John') {
+//     // break;
+//     continue;
+//   }
+//   console.log(name);
+// }
+
+// ========================================
+/* Массивы - Многомерные массивы */
+// ========================================
