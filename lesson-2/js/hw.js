64fb3e87b83f2b7a7cc86653de3a41dd62d90a3a
belon-creator <belon8911@gmail.com>
2020-10-25 19:37:21 +0200

    add 2 tasks more

--- lesson-2/js/hw.js
+++ lesson-2/js/hw.js
@@ -12,3 +12,55 @@ const logItems = function (array) {
 logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+// ==================task2====================
+
+const calculateEngravingPrice = function (message, pricePerWord) {
+  let words = message.split(' ');
+  let sum = words.length * pricePerWord;
+
+  return sum;
+};
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10
+  )
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20
+  )
+); // 160
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)
+); // 240
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)
+); // 120
+
+// =============task-3================
+
+const findLongestWord = function (string) {
+  const words = string.split(' ');
+  let longestWord = words[0];
+
+  for (const word of words) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+
+  return longestWord;
+};
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
